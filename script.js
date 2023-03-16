2aaee1b73b22f48bcae675cbd9160cea74fa2b57
Rathin21 <114067851+Rathin21@users.noreply.github.com>
2023-03-16 12:40:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,34 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
-// You can write your code here
+function fetchData(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.error(`Error fetching data`));
+}
+
+// function to measure time taken for Promise.all
+function measurePromiseAll() {
+  const start = Date.now();
+  Promise.all(apiUrls.map(fetchData))
+    .then(() => {
+      const end = Date.now();
+      const timeTaken = end - start;
+      document.getElementById('output-all').innerHTML = `${timeTaken}ms`;
+    });
+}
+
+// function to measure time taken for Promise.any
+function measurePromiseAny() {
+  const start = Date.now();
+  Promise.any(apiUrls.map(fetchData))
+    .then(() => {
+      const end = Date.now();
+      const timeTaken = end - start;
+      document.getElementById('output-any').innerHTML = `${timeTaken}ms`;
+    });
+}
+
+// call the functions to measure performance
+measurePromiseAll();
+measurePromiseAny();
